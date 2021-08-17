ae2f984d13b5a09ee347de9ccb523519e57b0bce
umemiya <euthanasia045@gmail.com>
2021-08-17 17:11:05 +0900

    first commit

---
+++ src/main/java/umemiya/trumpapi/TrumpApiApplication.java
@@ -0,0 +1,13 @@
+package umemiya.trumpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrumpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrumpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umemiya/trumpapi/TrumpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package umemiya.trumpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrumpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
